469d30b47c75e8ded026a58dd9d39303757aac6e
Wessz <wesleye986@gmail.com>
2024-08-22 22:40:35 -0300

    AddIpUser

--- SpeedTestLocal.py
+++ SpeedTestLocal.py
@@ -10,8 +10,10 @@ def bytes_to_mb (bytes):
 speed_download = speed.download()
 speed_upload = speed.upload()
 server = speed.get_best_server()
-ip_address = server['host'].split(':')[0]
+ip_address_name = server['host'].split(':')[0]
+ip_address = speed.results.client['ip']
 
 print(f"Download: {bytes_to_mb(speed_download)}MB")
 print(f"Upload: {bytes_to_mb(speed_upload)}MB")
+print(f"IP_Name_Server: {ip_address_name}")
 print(f"IP: {ip_address}")
